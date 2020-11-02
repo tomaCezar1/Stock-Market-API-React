72963b82de7c0197113a5b90a2255f35f66f1cd4
Cezar <cesaretomo@hotmail.com>
2020-11-03 00:31:28 +0200

    Updates

---
+++ precache-manifest.98ac693202acb62b0f54cd992aacc90b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7627c445c695f7087612a7bc307db6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "846202b45e9588243adb",
+    "url": "/static/css/main.9ffef9ab.chunk.css"
+  },
+  {
+    "revision": "de6b20a83ac8556bedf2",
+    "url": "/static/js/2.e59a8887.chunk.js"
+  },
+  {
+    "revision": "27200a5499d660fd8253e73b0c4cf016",
+    "url": "/static/js/2.e59a8887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "846202b45e9588243adb",
+    "url": "/static/js/main.3aba7c09.chunk.js"
+  },
+  {
+    "revision": "ebb211239cf7dea5cf75",
+    "url": "/static/js/runtime-main.42108e94.js"
+  }
+]);
